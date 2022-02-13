8ae6d149ad540981e46a678d1b6326f2cc728383
Javier Azuara <javierazuara@me.com>
2022-02-13 15:56:56 -0500

    Initial Commit

---
+++ SiriShortcuts/SiriShortcutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiriShortcutsApp.swift
+//  SiriShortcuts
+//
+//  Created by Javier Azuara on 2/13/22.
+//
+
+import SwiftUI
+
+@main
+struct SiriShortcutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
